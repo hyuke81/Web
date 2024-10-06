f78d874786afb6623509d5b401f7c82dcc1c6245
hyuke81 <rmsidthgusdl81@gmail.com>
2024-10-06 12:17:34 +0900

    mission: submit week2 mission02

--- mission/chapter02/mission2/src/App.jsx
+++ mission/chapter02/mission2/src/App.jsx
@@ -1,16 +1,53 @@
-import './App.css';
-import { MOVIES } from './mocks/movies'; 
+import { useState } from 'react';
+import { MOVIES } from './mocks/movies';
 
 const App = () => {
+  const [clickedMovie, setClickedMovie] = useState(null);
+
+  const handleClicked = (movieId) => {
+    setClickedMovie((getMovieId) => (getMovieId === movieId ? null : movieId));
+  };
+
   return (
-    <div className="grid">
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(10, 1fr)',
+        justifyItems: 'center',
+        padding: '1px',
+        gap: '5px',
+      }}
+    >
       {MOVIES.results.slice(0, 20).map((movie) => (
-        <div key={movie.id} className="photo">
+        <div
+          key={movie.id}
+          style={{
+            position: 'relative',
+            width: '100px',
+            height: '150px',
+          }}
+          onClick={() => handleClicked(movie.id)}
+        >
           <img
             src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-            className="movie_card"
+            style={{
+              width: '100%',
+              height: '100%',
+              borderRadius: '5%',
+            }}
           />
-          <div className="blackbox"></div>
+          <div
+            style={{
+              position: 'absolute',
+              top: '0',
+              left: '0',
+              width: '100%',
+              height: '100%',
+              borderRadius: '5%',
+              backgroundColor: 'black',
+              opacity: clickedMovie === movie.id ? 0.5 : 0
+            }}
+          ></div>
         </div>
       ))}
     </div>
